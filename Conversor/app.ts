e9570af895dcb9db69a119601136c3d05bba6f8b
edusilvac <127169103+edusilvac@users.noreply.github.com>
2023-08-02 20:46:02 -0300

    Att

--- Conversor/app.ts
+++ Conversor/app.ts
@@ -1,104 +1,3 @@
-//SEM VÍRGULA
-/* import Conversor from './conversor';
-
-async function main() {
-  const conversor = new Conversor();
-
-  try {
-    const moedaOrigem = 'BRL';
-    const moedaDestino = 'USD';
-    const valor = 100;
-
-    const taxaDeConversao = await conversor.obterTaxaDeConversao(moedaOrigem, moedaDestino);
-    const valorConvertido = await conversor.converterValor(moedaOrigem, moedaDestino, valor);
-    const dataAtual = new Date().toLocaleDateString('pt-BR'); //Data atual no formato 'DD-MM-YYYY'
-
-    console.log("------------------------------------------");
-    console.log("Data da Conversão:", dataAtual); // Data atual
-    console.log("Moeda de Origem:", moedaOrigem);
-    console.log("Moeda de Destino:", moedaDestino);
-    console.log("------------------------------------------");
-    console.log("Valor:", valor);
-    console.log(`Valor Convertido: ${valorConvertido.toFixed(2)} ${moedaDestino}`);
-    console.log(`Taxa de conversão: ${moedaOrigem} => ${taxaDeConversao.taxa.toFixed(6)} => ${moedaDestino}`);
-    console.log("------------------------------------------");
-} catch (error: any) {
-    console.error('Erro ao converter valor:', error.message);
-  }
-}
-
-main(); */
-
-
-
-
-
-//ESTAMOS TRABALHANDO COM VÍRGULA AGORA
-/*import Conversor from './conversor';
-
-async function main() {
-  const conversor = new Conversor();
-
-  try {
-    const moedaOrigem = 'BRL';
-    const moedaDestino = 'USD';
-    const valor = 80.95;
-
-    const taxaDeConversao = await conversor.obterTaxaDeConversao(moedaOrigem, moedaDestino);
-    const valorConvertido = await conversor.converterValor(moedaOrigem, moedaDestino, valor);
-    const dataAtual = new Date().toLocaleDateString('pt-BR'); //Data atual no formato 'DD-MM-YYYY'
-
-    console.log("------------------------------------------");
-    console.log("Data da Conversão:", dataAtual); // Data atual
-    console.log("Moeda de Origem:", moedaOrigem);
-    console.log("Moeda de Destino:", moedaDestino);
-    console.log("------------------------------------------");
-    console.log("Valor:", valor.toLocaleString('pt-BR')); // Utilizamos toLocaleString com 'pt-BR' para formatar o número com vírgula
-    console.log(`Valor Convertido: ${valorConvertido.toLocaleString('pt-BR')} ${moedaDestino}`); // Utilizamos toLocaleString com 'pt-BR' para formatar o número com vírgula
-    console.log(`Taxa de conversão: ${moedaOrigem} => ${taxaDeConversao.taxa.toLocaleString('pt-BR')} => ${moedaDestino}`); // Utilizamos toLocaleString com 'pt-BR' para formatar o número com vírgula
-    console.log("------------------------------------------");
-  } catch (error: any) {
-    console.error('Erro ao converter valor:', error.message);
-  }
-}
-
-main();*/
-
-
-//TRABALHANDO COM PONTO E VÍRGULA
-/* import Conversor from './conversor';
-
-async function main() {
-  const conversor = new Conversor();
-
-  try {
-    const moedaOrigem = 'BRL';
-    const moedaDestino = 'USD';
-    const valorString = '100,99'; // Use uma string para manter a vírgula como separador decimal
-
-    // Substituir vírgulas por pontos antes de converter o valor em número
-    const valor = parseFloat(valorString.replace(',', '.'));
-
-    const taxaDeConversao = await conversor.obterTaxaDeConversao(moedaOrigem, moedaDestino);
-    const valorConvertido = await conversor.converterValor(moedaOrigem, moedaDestino, valor);
-    const dataAtual = new Date().toLocaleDateString('pt-BR'); //Data atual no formato 'DD-MM-YYYY'
-
-    console.log("------------------------------------------");
-    console.log("Data da Conversão:", dataAtual); // Data atual
-    console.log("Moeda de Origem:", moedaOrigem);
-    console.log("Moeda de Destino:", moedaDestino);
-    console.log("------------------------------------------");
-    console.log("Valor:", valor.toLocaleString('pt-BR')); // Utilizamos toLocaleString com 'pt-BR' para formatar o número com vírgula
-    console.log(`Valor Convertido: ${valorConvertido.toLocaleString('pt-BR')} ${moedaDestino}`); // Utilizamos toLocaleString com 'pt-BR' para formatar o número com vírgula
-    console.log(`Taxa de conversão: ${moedaOrigem} => ${taxaDeConversao.taxa.toLocaleString('pt-BR')} => ${moedaDestino}`); // Utilizamos toLocaleString com 'pt-BR' para formatar o número com vírgula
-    console.log("------------------------------------------");
-  } catch (error: any) {
-    console.error('Erro ao converter valor:', error.message);
-  }
-}
-
-main();*/
-
 import Conversor from './conversor';
 import prompt from 'prompt-sync';
 
@@ -117,6 +16,15 @@ async function main() {
     }
 
     const moedaDestino = promptSync('Digite a moeda de destino (ex: USD): ').toUpperCase();
+
+
+    // Verificar se as moedas de origem e destino são diferentes
+    if (moedaOrigem === moedaDestino) {
+      console.log("A moeda de origem não pode ser igual à moeda de destino.");
+      return;
+    }
+
+
     const valorString = promptSync('Digite o valor a ser convertido: ');
 
     // Verificar se as moedas são válidas
